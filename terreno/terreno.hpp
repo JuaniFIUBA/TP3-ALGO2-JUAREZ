e9bb4f8888fcc656954f9c364dd824d3e7760441
hernei152 <bsilvano@fi.uba.ar>
2022-11-28 12:48:31 -0300

    terreno

---
+++ terreno/terreno.hpp
@@ -0,0 +1,25 @@
+#ifndef TERRENO_HPP
+#define TERRENO_HPP
+
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include "colors.h"
+
+using namespace std;
+
+class Terreno{
+private:
+    static const int dimension = 8;
+    string terreno[dimension][dimension];
+    int mapa_de_consumo[dimension][dimension];
+    int asignar_consumo(string color);
+    int num_aleatorio();
+
+public:
+    Terreno();
+    void cargar_terreno();
+    void mostrar_terreno();
+};
+
+#endif
