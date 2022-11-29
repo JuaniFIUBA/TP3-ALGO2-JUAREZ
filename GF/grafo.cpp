210df0d7eef7724a358ff5f9b365fb37b46dccb5
Nicolas152 <nfunes@fi.uba.ar>
2022-11-29 18:59:01 -0300

    .

--- GF/grafo.cpp
+++ GF/grafo.cpp
@@ -374,34 +374,3 @@ int Grafo::Dijkstra(int inicio, int destino)
     return -1;      
 }
 
-/*
-int algoritmo(int begin, int end)
-	{
-		priority_queue<State> pq; // La cola de prioridad.
-		vector<int> Dist(graph.V, oo); // La distancia hacia todos los vertices. Inicialmente para cada vertice su valor es infinito.
-		vector<bool> mark(graph.V, false); // Este arreglo nos permitira determinar los nodos procesados.
-	
-		Dist[begin] = 0; // Valor inicial del vertice de partida.
-		pq.push(State(begin, 0)); // Agregamos el primer elemento, que no es mas que el vertice de partida.
-		while(!pq.empty()) // Mientras existan vertices por procesar.
-		{
-			State st = pq.top(); pq.pop(); // Se desencola el elemento minimo.
-			mark[st.node] = true; // Se marca el nodo como visitado.
-			if (st.node == end)
-				return st.cost; // Retornamos el valor del camino, hemos llegado al vertice destino.
-	
-			int T = (int)graph.G[st.node].size();
-			for(int i = 0; i < T; ++i) // Se recorren las adyacencias de "a".
-			{
-				// Si no ha sido procesado el vertice "vi" y la distancia hacia "vi" es menor a la distancia
-				// en Dist entonces hemos encontrado un camino mas corto a "vi".
-				if (!mark[graph.G[st.node][i].node] && ((Dist[st.node] + graph.G[st.node][i].cost) < Dist[graph.G[st.node][i].node]))
-				{
-					Dist[graph.G[st.node][i].node] = st.cost + graph.G[st.node][i].cost;
-					pq.push(State(graph.G[st.node][i].node, st.cost + graph.G[st.node][i].cost));
-				}
-			}
-		}
-		return -1; // Si no se puede llegar al destino, retornar -1.
-	}
-*/
