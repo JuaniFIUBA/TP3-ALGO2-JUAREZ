06b4de80b49e20beb59ec939e0a69f7a9b4c69f6
LeanFIUBA <lpenaloza@fi.uba.ar>
2022-11-30 23:59:39 -0300

    Un par de comentarios

--- GF/mapa.hpp
+++ GF/mapa.hpp
@@ -15,10 +15,18 @@ class Mapa
 
         //Pre: Recibe la cantidad de filas y columnas
         //Pro: Une los vertices para crear casilleros para un mapa de fila x columna.Tiene que ser matriz cuadrada.
-        void unir_casilleros();      
+        void unir_casilleros();  
+
+        //Pre:-
+        //Post: Modifica el mapa en pantalla
         void editar_mapa();
+
+        //Pre:-
+        //Post: Muestra el mapa en pantalla
         void mostrar_mapa();
-        void generar_mapa();
+
+        //Pre:-
+        //Post: Calcula el camino minimo a recorrer de origen a destino.
         void camino_minimo();
 };
 
