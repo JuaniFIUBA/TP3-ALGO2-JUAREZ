8a4132f879a8d7aeba72df4986457206827c2bc6
hernei152 <bsilvano@fi.uba.ar>
2022-12-01 21:39:31 -0300

    .

--- Grafo/terreno.cpp
+++ Grafo/terreno.cpp
@@ -111,6 +111,15 @@ void Terreno::eliminar_animal(int fil_auto, int col_auto){
     }
 }
 
+void Terreno::colocar_indices(){
+    for(int t = 0; t < dimension; t++){
+        terreno[t][0] = t;
+    }
+    for(int u = 0; u < dimension; u++){
+        terreno[0][u] = u;
+    }
+}
+
 void Terreno::inicializar_terreno(){
     cargar_mapa_consumo();
     cargar_colores();
@@ -125,6 +134,7 @@ void Terreno::actualizar_terreno(int fil_orig, int col_orig, int fil_dest, int c
 }
 
 void Terreno::mostrar_terreno(){
+    colocar_indices();
     for(int t = 0; t < dimension; t++){
         cout<< t;
     }
